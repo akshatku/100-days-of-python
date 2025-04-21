7320a54f14548fdce7186b80f7a8c0cd004d5e5e
Akshat Kumar <64675922+akshatku@users.noreply.github.com>
2025-04-21 14:35:58 -0400

    Update rock_paper_scissors.py

--- Day04/rock_paper_scissors.py
+++ Day04/rock_paper_scissors.py
@@ -1,1 +1,72 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+options_list = [rock, paper, scissors]
+
+comp_choice = random.choice(options_list)
+
+if user_choice not in [0, 1, 2]:
+    print("Invalid choice")
+else:
+    if user_choice == 0 and comp_choice == scissors:
+        print(options_list[0])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("You win!")
+    elif user_choice == 2 and comp_choice == rock:
+        print(options_list[2])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("You lose")
+    elif user_choice == 1 and comp_choice == scissors:
+        print(options_list[1])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("You lose")
+    elif user_choice == 2 and comp_choice == paper:
+        print(options_list[2])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("You win!")
+    elif user_choice == 0 and comp_choice == paper:
+        print(options_list[0])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("You lose")
+    elif user_choice ==1 and comp_choice == rock:
+        print(options_list[1])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("You win!")
+    else:
+        print(options_list[user_choice])
+        print("\nComputer Chose:\n")
+        print(comp_choice)
+        print("It's a draw")
